27e020cd5f0d7c44ed41ff25621505bcc68111aa
Dhruv Mehta <dhruvmehta1311@gmail.com>
2024-08-24 12:27:00 +0530

    Changed Width..

--- src/App.jsx
+++ src/App.jsx
@@ -15,28 +15,28 @@ function App() {
     toast.info("This is an Info Message..");
   }
   return (
-    <div className="bg-gray-800 w-full h-screen text-white p-10 flex items-center">
-      <div className="max-w-[700px] w-[90%] mx-auto flex flex-col items-center gap-4">
+    <div className="bg-gray-800 w-full h-screen text-white flex items-center">
+      <div className="max-w-[700px] w-full mx-auto flex flex-col items-center gap-4">
         <button
-          className="bg-green-600 px-2 py-4 rounded w-1/2"
+          className="bg-green-600 px-2 py-4 rounded w-[270px] md:w-1/2"
           onClick={successMessage}
         >
           Click here for Success Message
         </button>
         <button
-          className="bg-red-600 px-2 py-4 rounded w-1/2"
+          className="bg-red-600 px-2 py-4 rounded w-[270px] md:w-1/2"
           onClick={errorMessage}
         >
           Click here for Error Message
         </button>
         <button
-          className="bg-yellow-600 px-2 py-4 rounded w-1/2"
+          className="bg-yellow-600 px-2 py-4 rounded w-[270px] md:w-1/2"
           onClick={warningMessage}
         >
           Click here for Warning Message
         </button>
         <button
-          className="bg-blue-600 px-2 py-4 rounded w-1/2"
+          className="bg-blue-600 px-2 py-4 rounded w-[270px] md:w-1/2"
           onClick={infoMessage}
         >
           Click here for Info Message
